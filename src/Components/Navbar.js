3c328188f0c4faa211d05e5f6128a10eefc9078b
developingright <mshaheer2003@gmail.com>
2023-03-06 20:59:43 +0530

    coded basic layout for the navbar and created navbar.module.css for navbar specific styles

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import * as styles from '../styles/navbar.module.css'
 
 export default function Navbar() {
   return (
-    <div>
-      This is Navbar Component
-    </div>
+    <nav className={styles.navbar}>
+      <label className={styles.logo}>Training Center</label>
+      <ul className={styles.nav_ul}>
+        <li className={styles.nav_li}>
+          <Link to='/' className={styles.nav_links}>About</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
